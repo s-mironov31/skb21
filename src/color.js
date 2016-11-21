1f805a71a14067244840f2ffe835bec89d32f8b3
s.mironov <s.mironov3112@gmail.com>
2016-11-21 13:34:21 +0300

    task2D

--- src/color.js
+++ src/color.js
@@ -1,10 +1,43 @@
-export default function hexColor(color) {
-  if(!color) return 'Invalid color';
-  // const re = /(?:([a-zA-Z0-9а-яА-ЯёЁ]*)\s+)?(?:([a-zA-Z0-9а-яА-ЯёЁ]*)\s+)?([a-zA-Z0-9а-яА-ЯёЁ]*)/;
-  const re = /(?:^(?:\s*)?#?([a,b,c,d,e,f,\d]{6}|[a,b,c,d,e,f,\d]{3})?(?:\s*)?$)?/i;
-  console.log(color.match(re));
+import {color} from 'kewler';
 
-  let hexcolor = color.match(re)[1];
+export default function hexColor(reqColor) {
+  if(!reqColor) return 'Invalid color';
+  console.log(reqColor);
+  const re = /(?:\s*(?:%20)*(rgb|hsl)(?:\()\s*(?:%20)*(\d{1,3})\s*(?:%20)*,\s*(?:%20)*(\d{1,3})(%)?\s*(?:%20)*,\s*(?:%20)*(\d{1,3})(%)?\s*(?:%20)*(?:\)))?(?:^\s*#?([a-f,\d]{6}|[a-f,\d]{3})?(?:\s*)?$)?/i;
+  console.log(reqColor.match(re));
+  // rgb(0, 255, 64)
+  // hsl(195, 100%, 50%)
+
+  let hexcolor;
+  const reHexColor = reqColor.match(re);
+  if(reHexColor[2]) {
+    if(reHexColor[1] == "rgb") {
+      let r = (reHexColor[2] * 1);
+      let g = (reHexColor[3] * 1);
+      let b = (reHexColor[5] * 1);
+      if((r < 0 || r > 255) || (g < 0 || g > 255) || (b < 0 || b > 255))
+        return 'Invalid color';
+
+      r = r.toString(16);
+      g = g.toString(16);
+      b = b.toString(16);
+
+
+      hexcolor = (r.length == 1 ? r + r : r) + (g.length == 1 ? g + g : g) + (b.length == 1 ? b + b : b);
+    } else {
+      if(!reHexColor[4] || !reHexColor[6])
+        return 'Invalid color';
+      const hsl = [+reHexColor[2], +reHexColor[3], +reHexColor[5]]
+      if((hsl[1] < 0 || hsl[1] > 100) || (hsl[2] < 0 || hsl[2] > 100))
+        return 'Invalid color';
+
+      const blueFromHSLArray = color(hsl);
+      hexcolor = blueFromHSLArray().slice(1);
+    }
+  } else  if(reHexColor[7]) {
+    hexcolor = reHexColor[7];
+  }
+
   if(!hexcolor) return 'Invalid color';
 
   if(hexcolor.length == 3)
